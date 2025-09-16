bd53939f4ac283404cf68f7d9aa20c10bccbc8fe
Durga Prasad <saikamireddi3@gmail.com>
2025-09-16 20:07:18 +0530

    name changed

--- src/toolkit/userSlice.js
+++ src/toolkit/userSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { saveToLocalStorage } from "../helpers/Localstorage";
+import { saveToLocalStorage } from "../helpers/localstorage";
 const initialState = {
   loading: false,
   user: {
